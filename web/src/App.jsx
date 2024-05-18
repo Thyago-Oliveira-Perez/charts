f2347e9ae0d16e2db0597b22149a09db714220a6
Thyago-Oliveira-Perez <thyagodoliveiraperez@gmail.com>
2024-05-18 20:57:38 -0300

    feat: init communication between FE and BE

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,3 +1,12 @@
+import Api from "./api/api";
+
 export default function App() {
+  const api = new Api();
+
+  const getInfos = async () => {
+    const response = await api.get("infos");
+    console.log(response);
+  };
+
   return <>Hello World!</>;
 }
---
+++ web/src/api/api.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export default class Api {
+  url = import.meta.VITE_API_URL;
+
+  axiosClient = axios.create({
+    baseURL: this.url,
+    headers: { "Content-type": "application/json" },
+  });
+
+  async get(path, options = {}) {
+    return await this.axiosClient.get(`${this.url}/${path}`, options);
+  }
+}
