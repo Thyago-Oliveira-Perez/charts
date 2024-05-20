2712da60df92d28663db72c0b73afe506519a650
Thyago-Oliveira-Perez <thyagodoliveiraperez@gmail.com>
2024-05-20 12:38:49 -0300

    feat: move to HomePage component

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,52 +1,10 @@
-import { useEffect, useState } from "react";
-import Api from "./api/api";
 import "./App.css";
-import LineChart from "./components/LineChart";
-import BarChart from "./components/BarChart";
-import DoughnutChart from "./components/DoughnutChart";
-import PieChart from "./components/PieChart";
-import Loading from "./components/Loading/Loading";
+import HomePage from "./pages/Home";
 
 export default function App() {
-  const [lineData, setLineData] = useState([65, 59, 80, 81, 56, 55, 40]);
-  const [barData, setBarData] = useState(null);
-  const [doughnutData, setDoughnutData] = useState([300, 50, 100]);
-  const [pirData, setPirData] = useState([300, 50, 100]);
-  const api = new Api();
-
-  useEffect(() => {
-    api.get("bar-chart").then((res) => {
-      setBarData(res.data);
-    });
-  }, []);
-
   return (
     <div className="container">
-      <h1>Cyber Security Charts</h1>
-      <div className="chart-grid">
-        <div className="chart-item">
-          <h2>Line Chart</h2>
-          <LineChart info={lineData} />
-        </div>
-        <div className="chart-item">
-          <h2>Bar Chart</h2>
-          {!barData ? (
-            <Loading />
-          ) : (
-            <>
-              <BarChart data={barData} />
-            </>
-          )}
-        </div>
-        <div className="chart-item">
-          <h2>Doughnut Chart</h2>
-          <DoughnutChart info={doughnutData} />
-        </div>
-        <div className="chart-item">
-          <h2>Pie Chart</h2>
-          <PieChart info={pirData} />
-        </div>
-      </div>
+      <HomePage />
     </div>
   );
 }
---
+++ web/src/pages/Home.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import LineChart from "../components/LineChart";
+import BarChart from "../components/BarChart";
+import DoughnutChart from "../components/DoughnutChart";
+import PieChart from "../components/PieChart";
+import Loading from "../components/Loading/Loading";
+import Api from "./../api/api";
+import "./Home.css";
+
+export default function HomePage() {
+  const [lineData, setLineData] = useState([65, 59, 80, 81, 56, 55, 40]);
+  const [barData, setBarData] = useState(null);
+  const [doughnutData, setDoughnutData] = useState([300, 50, 100]);
+  const [pirData, setPirData] = useState([300, 50, 100]);
+  const api = new Api();
+
+  useEffect(() => {
+    api.get("bar-chart").then((res) => {
+      setBarData(res.data);
+    });
+  }, []);
+
+  return (
+    <div className="container">
+      <h1>Cyber Security Charts</h1>
+      <div className="chart-grid">
+        <div className="chart-item">
+          <h2>Line Chart</h2>
+          <LineChart info={lineData} />
+        </div>
+        <div className="chart-item">
+          <h2>Bar Chart</h2>
+          {!barData ? (
+            <Loading />
+          ) : (
+            <>
+              <BarChart data={barData} />
+            </>
+          )}
+        </div>
+        <div className="chart-item">
+          <h2>Doughnut Chart</h2>
+          <DoughnutChart info={doughnutData} />
+        </div>
+        <div className="chart-item">
+          <h2>Pie Chart</h2>
+          <PieChart info={pirData} />
+        </div>
+      </div>
+    </div>
+  );
+}
